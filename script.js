6ee48d9f46a80ca8ab7706d921602588676fc59a
Ahmad Asadi Looyeh <asadi.loyeh@gmail.com>
2024-05-07 19:09:01 +0330

    first commit

---
+++ script.js
@@ -0,0 +1,17 @@
+let counters = document.querySelectorAll(".counter");
+counters = Array.from(counters);
+
+console.log(counters);
+
+window.addEventListener("load", () => {
+  let i = 0;
+  const interv = setInterval(() => {
+    counters[0].textContent = 120 * i;
+    counters[1].textContent = 50 * i;
+    counters[2].textContent = 75 * i;
+    i++;
+    if (i == 101) {
+      clearInterval(interv);
+    }
+  }, 10);
+});
